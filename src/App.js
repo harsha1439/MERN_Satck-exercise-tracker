b35f8557c43ddca52ef93e11ba1a77b7684291cb
Harsha <Z1856779@students.niu.edu>
2020-06-02 12:13:33 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="Ap">
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React in a faster way
         </a>
       </header>
     </div>
